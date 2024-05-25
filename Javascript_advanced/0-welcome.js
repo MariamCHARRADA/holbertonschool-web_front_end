af8763b835d2bb3f893396e8d5a8f071b1b4151b
MariamCHARRADA <charradamariam@yahoo.com>
2024-05-25 23:53:20 +0100

    add the exclamation mark

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,7 +1,8 @@
 function welcome (firstName, lastName){
-    let fullName = firstName + ' ' + lastName ;
+    let fullName = firstName + ' ' + lastName+'!'; ;
     function displayFullName(){
         alert(fullName);
+        console.log(fullName);
     }
     displayFullName();
 }
