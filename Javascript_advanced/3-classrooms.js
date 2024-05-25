ec7fdd5a0ec978816dbb9e08431a65c130feacbc
MariamCHARRADA <charradamariam@yahoo.com>
2024-05-26 00:13:20 +0100

    Closure and loops

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,14 @@
+function createClassRoom (numbersOfStudents){
+function studentSeat (seat){
+    return function(){
+        return seat
+    }
+}
+
+let students = []
+for (let i = 1; i <= numbersOfStudents; i++){
+    students.push(studentSeat(i))
+}
+return students
+} 
+classRoom = createClassRoom(10)
